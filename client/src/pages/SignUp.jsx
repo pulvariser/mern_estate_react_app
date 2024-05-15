bc12c668e0c7a082a1db97221ce83b012d323717
er Mayur <mayurburange72@gmail.com>
2024-05-15 22:00:17 +0530

    OAuth fixed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {Link, useNavigate} from "react-router-dom"
-import OAuth from '../components/Oauth';
+import OAuth from '../components/OAuth';
 
 const SignUp = () => {
   const [formData, setFormData]=useState({});
